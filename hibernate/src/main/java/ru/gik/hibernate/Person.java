ca92e2598a5c3faad2709539297a39aac8abc068
KonstantinGlady <gladyi@hotmail.com>
2020-07-04 15:34:20 +0300

    hibernate

---
+++ hibernate/src/main/java/ru/gik/hibernate/Person.java
@@ -0,0 +1,52 @@
+package ru.gik.hibernate;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    public Person() {
+
+    }
+
+    public Person(String name, String surname) {
+        this.firstname = name;
+        this.lastname = surname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
